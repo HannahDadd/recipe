0bf3c4cdf71c47e791abdd82b8fa8c478c307b22
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2019-03-06 10:52:05 +0000

    cleaned up logging

--- src/main/java/com/bbc/news/hannah/controller/Main.java
+++ src/main/java/com/bbc/news/hannah/controller/Main.java
@@ -4,12 +4,17 @@ import com.bbc.news.hannah.model.Book;
 import com.bbc.news.hannah.model.Food;
 import com.bbc.news.hannah.model.Recipe;
 import com.bbc.news.hannah.model.Tool;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+@Slf4j
 public class Main {
+
+
     public static void main(String[] args) {
+
 
         int foodId = 0;
         Food advocado = Food.builder().id(foodId).name("advocado").isLiquid(false).quantityUnit("avocados").quantity(1).build();
@@ -28,7 +33,6 @@ public class Main {
                 .build();
 
         Book book = Book.builder().id(1).authorName("Hannah").title("Book the First").recipe(recipe).build();
-
-        System.out.println(book.getRecipes().get(0).getRecipe());
+        log.info(book.getRecipes().get(0).getRecipe());
     }
 }
